409d403300b5d3a7f974ebee54863337d3e157c7
spalger <email@spalger.com>
2019-04-23 12:55:34 -0700

    remove debugging stuff

--- app.ts
+++ app.ts
@@ -2,20 +2,11 @@ import { createMicroHandler, Route } from '@spalger/micro-plus'
 
 module.exports = createMicroHandler({
   routes: [
-    new Route('GET', '/', async (ctx) => {
-      return {
-        status: 200,
-        body: {
-          hello: 'world',
-          url: ctx.baseUrl,
-          foo: [
-            ctx.method,
-            ctx.pathname,
-            ctx.query,
-            ctx.url,
-          ]
-        }
+    new Route('GET', '/', async () => ({
+      status: 200,
+      body: {
+        hello: 'world',
       }
-    })
+    }))
   ]
 })
