78a1f40dbc652cbdb824e87ea201b2d850c75c9b
spalger <email@spalger.com>
2019-04-23 12:45:35 -0700

    test ctx parsed url opts

--- app.ts
+++ app.ts
@@ -2,11 +2,18 @@ import { createMicroHandler, Route } from '@spalger/micro-plus'
 
 module.exports = createMicroHandler({
   routes: [
-    new Route('GET', '/', async () => {
+    new Route('GET', '/', async (ctx) => {
       return {
         status: 200,
         body: {
-          hello: 'world'
+          hello: 'world',
+          url: ctx.baseUrl,
+          foo: [
+            ctx.method,
+            ctx.pathname,
+            ctx.query,
+            ctx.url,
+          ]
         }
       }
     })
