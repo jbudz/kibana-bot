0d85e15663781f98e4f94e985b8ed31abaff2426
spalger <email@spalger.com>
2019-04-23 12:42:30 -0700

    fix handler load

--- index.js
+++ index.js
@@ -16,6 +16,6 @@ require('elastic-apm-node').start({
 })
 
 require('ts-node/register')
-require('micro')(require('./index')).listen(8000, () => {
+require('micro')(require('./app')).listen(8000, () => {
   console.log('listening on port 8000')
 })
