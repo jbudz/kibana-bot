8fb3b0fbfa0fd744d86a0f302766d80fdfdb410a
spalger <email@spalger.com>
2019-04-24 10:24:11 -0700

    handle large request bodies a little better

--- app.ts
+++ app.ts
@@ -1,21 +1,7 @@
-import * as Crypto from 'crypto'
-
-import {
-  createMicroHandler,
-  Route,
-  getConfigVar,
-  UnauthorizedError,
-  ServerError,
-} from '@spalger/micro-plus'
+import { createMicroHandler, Route } from '@spalger/micro-plus'
 import * as apm from 'elastic-apm-node'
 
-const WEBHOOK_SECRET = getConfigVar('GITHUB_WEBHOOK_SECRET')
-const BRANCH_REF_TAG = 'refs/heads/'
-
-const getHmac = (string: string) =>
-  Crypto.createHmac('sha1', WEBHOOK_SECRET)
-    .update(string)
-    .digest('hex')
+import { webhookRoute } from './webhook'
 
 module.exports = createMicroHandler({
   routes: [
@@ -25,63 +11,7 @@ module.exports = createMicroHandler({
         hello: 'world',
       },
     })),
-
-    new Route('POST', '/webhook', async ctx => {
-      const body = await ctx.readBodyAsText()
-
-      const signature = ctx.header('X-Hub-Signature')
-      const expectedSignature = `sha1=${getHmac(body)}`
-      if (expectedSignature !== signature) {
-        console.log('INVALID WEBHOOK SIGNATURE %j', {
-          signature,
-          expectedSignature,
-        })
-        throw new UnauthorizedError('invalid webhook signature')
-      }
-
-      const event = ctx.header('X-GitHub-Event')
-      const webhook = JSON.parse(body)
-
-      switch (event) {
-        case 'push':
-          const { ref } = webhook
-          if (!ref.startsWith(BRANCH_REF_TAG)) {
-            return {
-              body: {
-                ignored: true,
-                reason: 'not a branch?',
-              },
-            }
-          }
-
-          const branch = ref.replace(BRANCH_REF_TAG, '')
-          return {
-            body: {
-              branch,
-            },
-          }
-
-        case 'pull_request':
-          const { pull_request: pr } = webhook
-          return {
-            body: {
-              number: pr.number,
-              state: pr.state,
-            },
-          }
-
-        case 'ping':
-          return {
-            statusCode: 200,
-            body: {
-              ignored: true,
-            },
-          }
-
-        default:
-          throw new ServerError(`unhandled event [${event}]`)
-      }
-    }),
+    webhookRoute,
   ],
   apmAgent: {
     onRequest() {},
