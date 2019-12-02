7d86d46edd8d4fe1c33d5ee15da487481e96c026
spalger <email@spalger.com>
2019-12-02 12:23:13 -0700

    add slack auth callback route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import { webhookRoute } from './webhook_route'
 import { helloWorldRoute } from './hello_world'
 import { refreshExpiredOutdatedRoute } from './refresh_expired_outdated'
+import { slackAuthCallbackRoute } from './slack_auth_callback'
 
 export const routes = [
   helloWorldRoute,
   webhookRoute,
   refreshExpiredOutdatedRoute,
+  slackAuthCallbackRoute,
 ]
---
+++ src/routes/slack_auth_callback.ts
@@ -0,0 +1,21 @@
+// import { getEsClient } from '../lib'
+import { Route, SearchParamError } from '@spalger/micro-plus'
+
+export const slackAuthCallbackRoute = new Route(
+  'GET',
+  '/slack_auth_callback',
+  async ctx => {
+    // const es = getEsClient(ctx)
+    const code = ctx.query['code']
+
+    if (!code || typeof code !== 'string') {
+      throw new SearchParamError('code', 'expected a single ?code= param')
+    }
+
+    return {
+      body: {
+        code,
+      },
+    }
+  },
+)
