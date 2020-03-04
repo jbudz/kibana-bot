d7e6404fd4b24965a60c62f3c96d8978ebd48257
spalger <spalger@users.noreply.github.com>
2020-03-04 08:12:55 -0700

    when commits are pushed set the push time as a commit status

--- src/reactors/push/store_push_time.ts
+++ src/reactors/push/store_push_time.ts
@@ -9,7 +9,7 @@ export const storePushTime = new Reactor<GithubWebhookPushEvent>({
     !input.deleted &&
     RELEASE_BRANCH_RE.test(input.ref.replace('refs/heads/', '')),
 
-  async exec({ input, es, log }) {
+  async exec({ input, es, log, githubApi }) {
     const body = []
 
     if (input.forced) {
@@ -19,9 +19,25 @@ export const storePushTime = new Reactor<GithubWebhookPushEvent>({
       })
     }
 
+    const setStatusPromises: Array<Promise<void>> = []
+
     for (const commit of input.commits) {
+      const pushTime = new Date()
       body.push({ create: { _id: commit.id } })
-      body.push({ pushTime: new Date() })
+      body.push({ pushTime })
+
+      setStatusPromises.push(
+        githubApi
+          .setCommitStatus(commit.id, {
+            context: 'prbot:storeCommitTime',
+            state: 'success',
+            description: `push time: ${pushTime.toISOString()}`,
+          })
+          .then(
+            () => undefined,
+            () => undefined,
+          ),
+      )
     }
 
     if (!body.length) {
@@ -30,9 +46,13 @@ export const storePushTime = new Reactor<GithubWebhookPushEvent>({
       }
     }
 
-    return await es.bulk({
-      index: 'prbot-commit-times',
-      body,
-    })
+    try {
+      return await es.bulk({
+        index: 'prbot-commit-times',
+        body,
+      })
+    } finally {
+      await Promise.all(setStatusPromises)
+    }
   },
 })
