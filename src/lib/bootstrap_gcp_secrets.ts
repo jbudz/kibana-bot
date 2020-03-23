d1c38c17843e89bcd454d8d79111845c9bb08264
Brian Seeders <brian.seeders@elastic.co>
2020-03-23 16:52:15 -0400

    WIP bootstrap gcp secrets

---
+++ src/lib/bootstrap_gcp_secrets.ts
@@ -0,0 +1,40 @@
+import { SecretManagerServiceClient } from '@google-cloud/secret-manager'
+import { log } from './log'
+
+export const GCP_SECRET_MAPPING: Record<string, string> = {
+  DIRECT_API_PASSWORD: 'kibana-bot-test-secret',
+  GITHUB_WEBHOOK_SECRET: 'kibana-bot-test-secret',
+  ES_URL: 'kibana-bot-test-secret-url',
+}
+
+const getSecret = async (client: any, id: string) => {
+  const [accessResponse] = await client.accessSecretVersion({
+    name: `projects/261553193300/secrets/${id}/versions/latest`,
+  })
+
+  return accessResponse?.payload?.data?.toString()
+}
+
+export async function bootstrapGcpSecrets() {
+  if (process.env.BOOTSTRAP_GCP_SECRETS !== 'true') {
+    return
+  }
+
+  const client = new SecretManagerServiceClient()
+  const envVars = Object.keys(GCP_SECRET_MAPPING).filter(
+    key => !(key in process.env),
+  )
+
+  try {
+    const values = await Promise.all(
+      envVars.map(key => getSecret(client, GCP_SECRET_MAPPING[key])),
+    )
+
+    envVars.forEach((envVar, index) => {
+      process.env[envVar] = values[index]
+    })
+  } catch (ex) {
+    log.error('Error bootstrapping secrets from GCP', ex)
+    process.exit(1)
+  }
+}
--- src/run_server.ts
+++ src/run_server.ts
@@ -1,13 +1,24 @@
 import micro from 'micro'
 
-import { log } from './lib'
-import { app } from './app'
+import { log } from './lib/log'
+import { bootstrapGcpSecrets } from './lib/bootstrap_gcp_secrets'
 
-micro(app(log))
-  .listen(8000, () => {
-    log.info('listening on port 8000')
-  })
-  .on('error', error => {
+async function start() {
+  try {
+    await bootstrapGcpSecrets()
+
+    micro(require('./app').app(log))
+      .listen(8000, () => {
+        log.info('listening on port 8000')
+      })
+      .on('error', error => {
+        log.error(error)
+        process.exit(1)
+      })
+  } catch (error) {
     log.error(error)
     process.exit(1)
-  })
+  }
+}
+
+start()
