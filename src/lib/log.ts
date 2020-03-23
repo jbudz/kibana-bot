68853db464488df661b27a2c9a23285731ee27fa
spalger <spalger@users.noreply.github.com>
2020-03-23 16:45:33 -0700

    make the divide a little clearer

--- src/lib/log.ts
+++ src/lib/log.ts
@@ -12,61 +12,53 @@ const filterRequestType = winston.format((info: any) =>
   info.type === 'request' ? false : info,
 )
 
-const getTransports = (es: Client | null): Transport[] => {
-  if (process.env.NODE_ENV !== 'production') {
-    return [
-      new winston.transports.Console({
-        format: winston.format.combine(
-          filterRequestType(),
-          winston.format.simple(),
-        ),
-      }),
-    ]
-  }
+const getDevTransports = () => [
+  new winston.transports.Console({
+    format: winston.format.combine(
+      filterRequestType(),
+      winston.format.simple(),
+    ),
+  }),
+]
 
-  if (!es) {
-    return [new winston.transports.Console()]
-  }
+const getProdTransports = (es: Client) => [
+  new winston.transports.Console(),
+  new WinstonElasticsearch({
+    index: 'kibana-bot-logs',
+    ensureMappingTemplate: false,
+    flushInterval: 500,
+    transformer(event) {
+      const message: string = event.message
+      const level: string = event.level || 'info'
+      const {
+        '@type': type = `level:${level}`,
+        extra = {},
+        ...restOfMeta
+      } = event.meta
+      const timestamp: string = event.timestamp!
 
-  return [
-    new winston.transports.Console(),
-    new WinstonElasticsearch({
-      index: 'kibana-bot-logs',
-      ensureMappingTemplate: false,
-      flushInterval: 500,
-      transformer(event) {
-        const message: string = event.message
-        const level: string = event.level || 'info'
-        const {
-          '@type': type = `level:${level}`,
-          extra = {},
-          ...restOfMeta
-        } = event.meta
-        const timestamp: string = event.timestamp!
-
-        // prevent objects from being used in meta, move them to extra
-        for (const key of Object.keys(restOfMeta)) {
-          if (typeof restOfMeta[key] === 'object' && restOfMeta[key] !== null) {
-            extra[key] = restOfMeta[key]
-            delete restOfMeta[key]
-          }
+      // prevent objects from being used in meta, move them to extra
+      for (const key of Object.keys(restOfMeta)) {
+        if (typeof restOfMeta[key] === 'object' && restOfMeta[key] !== null) {
+          extra[key] = restOfMeta[key]
+          delete restOfMeta[key]
         }
+      }
 
-        return {
-          '@type': type,
-          '@level': level,
-          '@message': message,
-          '@timestamp': timestamp,
-          meta: restOfMeta,
-          // wrap extra in an array so they're rendered as a single row in Kibana
-          extra: [extra],
-        }
-      },
-      client: es,
-      ...({ apm } as any),
-    }),
-  ]
-}
+      return {
+        '@type': type,
+        '@level': level,
+        '@message': message,
+        '@timestamp': timestamp,
+        meta: restOfMeta,
+        // wrap extra in an array so they're rendered as a single row in Kibana
+        extra: [extra],
+      }
+    },
+    client: es,
+    ...({ apm } as any),
+  }),
+]
 
 export type Log = winston.Logger
 
@@ -81,7 +73,10 @@ export function createRootLog(es: Client | null): Log {
       }),
       winston.format.json(),
     ),
-    transports: getTransports(es),
+    transports:
+      es && process.env.NODE_ENV === 'production'
+        ? getProdTransports(es)
+        : getDevTransports(),
   })
 }
 
