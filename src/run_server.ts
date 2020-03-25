f58a8c3817f283e5ff5bfe1ced09e45430d36b43
Brian Seeders <brian.seeders@elastic.co>
2020-03-25 10:39:19 -0400

    Refactor run_server a bit and add standard PORT support

--- src/run_server.ts
+++ src/run_server.ts
@@ -3,22 +3,27 @@ import micro from 'micro'
 import { bootstrapGcpSecrets } from './lib/bootstrap_gcp_secrets'
 import { app } from './app'
 
+const PORT = process.env.PORT || 8000
+
 async function start() {
   try {
     await bootstrapGcpSecrets()
     const { handler, log } = app()
 
-    micro(handler)
-      .listen(8000, () => {
-        log.info('listening on port 8000')
-      })
-      .on('error', error => {
-        log.error(error)
-        process.exit(1)
-      })
+    await new Promise((resolve, reject) => {
+      micro(handler)
+        .listen(PORT, () => {
+          log.info(`listening on port ${PORT}`)
+        })
+        .on('error', reject)
+        .on('close', resolve)
+    })
   } catch (error) {
     console.error(error)
+    process.exit(1)
   }
+
+  process.exit(0)
 }
 
 start()
