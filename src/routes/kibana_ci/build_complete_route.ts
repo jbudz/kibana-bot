20ac0c4e0c57ef091f65de77915ab261613c0938
spalger <spalger@users.noreply.github.com>
2020-04-21 20:43:47 -0700

    don't hide 404s in _complete endpoint

--- src/routes/kibana_ci/build_complete_route.ts
+++ src/routes/kibana_ci/build_complete_route.ts
@@ -13,28 +13,23 @@ export const buildCompleteRoute = new Route(
     }
 
     const es = getEsClient(ctx)
-    await es.update(
-      {
-        index: 'kibana-ci-stats__builds',
-        id: buildId,
-        body: {
-          script: {
-            lang: 'painless',
-            source: `
+    await es.update({
+      index: 'kibana-ci-stats__builds',
+      id: buildId,
+      body: {
+        script: {
+          lang: 'painless',
+          source: `
             if (ctx._source.completedAt == null) {
               ctx._source.completedAt = params.date
             }
           `,
-            params: {
-              date: new Date(),
-            },
+          params: {
+            date: new Date(),
           },
         },
       },
-      {
-        ignore: [404],
-      },
-    )
+    })
 
     return {
       status: 200,
