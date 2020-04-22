02fbaac40204206462248ac948b18e146833b046
spalger <spalger@users.noreply.github.com>
2020-04-21 20:49:32 -0700

    wrap es 404 errors

--- src/routes/kibana_ci/build_complete_route.ts
+++ src/routes/kibana_ci/build_complete_route.ts
@@ -1,4 +1,5 @@
-import { Route, BadRequestError } from '@spalger/micro-plus'
+import { Route, BadRequestError, NotFoundError } from '@spalger/micro-plus'
+import { errors } from '@elastic/elasticsearch'
 
 import { getEsClient } from '../../lib'
 import { requireApiKey } from '../../lib/kibana_ci'
@@ -13,23 +14,31 @@ export const buildCompleteRoute = new Route(
     }
 
     const es = getEsClient(ctx)
-    await es.update({
-      index: 'kibana-ci-stats__builds',
-      id: buildId,
-      body: {
-        script: {
-          lang: 'painless',
-          source: `
-            if (ctx._source.completedAt == null) {
-              ctx._source.completedAt = params.date
-            }
-          `,
-          params: {
-            date: new Date(),
+    try {
+      await es.update({
+        index: 'kibana-ci-stats__builds',
+        id: buildId,
+        body: {
+          script: {
+            lang: 'painless',
+            source: `
+              if (ctx._source.completedAt == null) {
+                ctx._source.completedAt = params.date
+              }
+            `,
+            params: {
+              date: new Date(),
+            },
           },
         },
-      },
-    })
+      })
+    } catch (error) {
+      if (error instanceof errors.ResponseError && error.statusCode === 404) {
+        throw new NotFoundError('unknown build')
+      }
+
+      throw error
+    }
 
     return {
       status: 200,
