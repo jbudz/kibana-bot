81418ae7386f560371c1d0ae78f36ea198ac3ef2
spalger <spalger@users.noreply.github.com>
2020-07-31 17:26:57 -0700

    only apply eslint to ts files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,28 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
-  extends: [
-    'prettier',
-    'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
-  ],
-  plugins: ['prettier'],
-  rules: {
-    'prettier/prettier': 'error',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/no-parameter-properties': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/no-non-null-assertion': 'off',
-    '@typescript-eslint/no-empty-function': 'off',
-    '@typescript-eslint/camelcase': [
-      'error',
-      {
-        properties: 'never',
+  overrides: [
+    {
+      files: ['**/*.ts'],
+      extends: [
+        'prettier',
+        'plugin:@typescript-eslint/recommended',
+        'prettier/@typescript-eslint',
+      ],
+      plugins: ['prettier'],
+      rules: {
+        'prettier/prettier': 'error',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/no-parameter-properties': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-non-null-assertion': 'off',
+        '@typescript-eslint/no-empty-function': 'off',
+        '@typescript-eslint/camelcase': [
+          'error',
+          {
+            properties: 'never',
+          },
+        ],
       },
-    ],
-  },
+    },
+  ],
 }
