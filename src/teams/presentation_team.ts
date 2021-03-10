13f1a8d1208ae0a33cae54fa196b2c20517311a5
Clint Andrew Hall <clint@clintandrewhall.com>
2021-03-09 22:47:31 -0600

    Updating Team:Presentation rules

--- src/teams/presentation_team.ts
+++ src/teams/presentation_team.ts
@@ -1,5 +1,6 @@
 import { LabelTransform } from '../reactors/apply_label_transforms'
 
+const LABEL_TEAM_PREFIX = 'Team:'
 const LABEL_PRESENTATION_TEAM = 'Team:Presentation'
 const LABEL_CANVAS = 'Feature:Canvas'
 const LABEL_DASHBOARD = 'Feature:Dashboard'
@@ -7,11 +8,24 @@ const LABEL_IMPACT_PREFIX = 'impact:'
 const LABEL_IMPACT_NEEDS_ASSESSMENT = 'impact:needs-assessment'
 const LABEL_LOE_PREFIX = 'loe:'
 const LABEL_LOE_NEEDS_RESEARCH = 'loe:needs-research'
+const LABEL_BUG = 'bug'
 
 const hasCanvasOrDashboardLabel = (labels: string[]) =>
   labels.some(label => label === LABEL_CANVAS || label === LABEL_DASHBOARD)
 
-const shouldAddPresentationTeamLabel: LabelTransform = labels =>
+const hasTeamLabel = (labels: string[]) =>
+  labels.some(label => label.startsWith(LABEL_TEAM_PREFIX))
+
+const isBug = (labels: string[]) => labels.some(label => label === LABEL_BUG)
+
+const shouldAddPresentationTeamLabelToIssue: LabelTransform = labels =>
+  hasCanvasOrDashboardLabel(labels) &&
+  !hasTeamLabel(labels) &&
+  !labels.some(label => label === LABEL_PRESENTATION_TEAM)
+    ? [...labels, LABEL_PRESENTATION_TEAM]
+    : labels
+
+const shouldAddPresentationTeamLabelToPR: LabelTransform = labels =>
   hasCanvasOrDashboardLabel(labels) &&
   !labels.some(label => label === LABEL_PRESENTATION_TEAM)
     ? [...labels, LABEL_PRESENTATION_TEAM]
@@ -19,6 +33,7 @@ const shouldAddPresentationTeamLabel: LabelTransform = labels =>
 
 const shouldAddImpactLabel: LabelTransform = labels =>
   hasCanvasOrDashboardLabel(labels) &&
+  isBug(labels) &&
   !labels.some(label => label.startsWith(LABEL_IMPACT_PREFIX))
     ? [...labels, LABEL_IMPACT_NEEDS_ASSESSMENT]
     : labels
@@ -31,6 +46,7 @@ const needsImpactLabel: LabelTransform = labels =>
 
 const shouldAddLOELabel: LabelTransform = labels =>
   hasCanvasOrDashboardLabel(labels) &&
+  isBug(labels) &&
   !labels.some(label => label.startsWith(LABEL_LOE_PREFIX))
     ? [...labels, LABEL_LOE_NEEDS_RESEARCH]
     : labels
@@ -42,11 +58,11 @@ const needsLOELabel: LabelTransform = labels =>
     : labels
 
 export const issueLabelTransforms = [
-  shouldAddPresentationTeamLabel,
+  shouldAddPresentationTeamLabelToIssue,
   shouldAddImpactLabel,
   shouldAddLOELabel,
 ]
 
 export const prMissingLabelTransforms = [needsImpactLabel, needsLOELabel]
 
-export const prAddLabelTransforms = [shouldAddPresentationTeamLabel]
+export const prAddLabelTransforms = [shouldAddPresentationTeamLabelToPR]
