f4a15520bc5fb51e2629edff28fc933dfae33f08
spalger <spencer@elastic.co>
2023-01-09 21:36:01 -0700

    ignore no-var-requires rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,11 +5,11 @@ module.exports = {
     'plugin:prettier/recommended',
   ],
   rules: {
-    'prettier/prettier': 'error',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-parameter-properties': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-non-null-assertion': 'off',
     '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
   },
 }
